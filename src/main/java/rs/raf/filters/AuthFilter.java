cd36c709df2c29ea7c39c29c8528b0b1405a16c5
Uroš Čolić <ucolic3021rn@raf.rs>
2024-05-29 22:25:14 +0200

    fixes CORS

--- src/main/java/rs/raf/filters/AuthFilter.java
+++ src/main/java/rs/raf/filters/AuthFilter.java
@@ -11,13 +11,15 @@ import rs.raf.services.UserService;
 import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.util.List;
 
 @Provider
-public class AuthFilter implements ContainerRequestFilter {
+public class AuthFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     @Inject
     UserService userService;
@@ -62,5 +64,13 @@ public class AuthFilter implements ContainerRequestFilter {
         System.out.println("No destination resource");
 
         return false;
+    }
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
     }
 }
